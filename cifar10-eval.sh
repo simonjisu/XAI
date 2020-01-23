30bc2177bcd067960eb0072912b6eac333838d5b
simonjisu <simonjisu@gmail.com>
2020-01-24 02:28:00 +0900

    ready to release new version except notebook and download weight files

--- cifar10-eval.sh
+++ cifar10-eval.sh
@@ -33,31 +33,12 @@ mkdir -p trainlog
 #     -sd 73 > ./trainlog/cifar10-resnetsmall-roar-rcd-eval.log &
 
 # fill global mean: 
-nohup python -u main.py \
-    -pp $HOME/code/XAI \
-    -dp $HOME/code/data \
-    -rf cifar10-resnetsmall-roar-rcd-fgm-eval \
-    -dt cifar10 \
-    -et roar \
-    -at random vanillagrad inputgrad guidedgrad gradcam \
-    -mt resnet resnetcbam resnetanr \
-    -down \
-    -bs 256 \
-    -ns 30 \
-    -cuda \
-    -skip1 \
-    -skip2 \
-    -fgm \
-    -rcd itu_r_bt2100 \
-    -sd 73 > ./trainlog/cifar10-resnetsmall-roar-rcd-fgm-eval.log &
-
-# kar version: do it after checked rcd is better
 # nohup python -u main.py \
 #     -pp $HOME/code/XAI \
 #     -dp $HOME/code/data \
-#     -rf cifar10-resnetsmall-kar-eval \
+#     -rf cifar10-resnetsmall-roar-rcd-fgm-eval \
 #     -dt cifar10 \
-#     -et kar \
+#     -et roar \
 #     -at random vanillagrad inputgrad guidedgrad gradcam \
 #     -mt resnet resnetcbam resnetanr \
 #     -down \
@@ -66,4 +47,23 @@ nohup python -u main.py \
 #     -cuda \
 #     -skip1 \
 #     -skip2 \
-#     -sd 73 > ./trainlog/cifar10-resnetsmall-kar-eval.log &
+#     -fgm \
+#     -rcd itu_r_bt2100 \
+#     -sd 73 > ./trainlog/cifar10-resnetsmall-roar-rcd-fgm-eval.log &
+
+# kar version: do it after checked rcd is better
+nohup python -u main.py \
+    -pp $HOME/code/XAI \
+    -dp $HOME/code/data \
+    -rf cifar10-resnetsmall-kar-rcd-eval \
+    -dt cifar10 \
+    -et kar \
+    -at random vanillagrad inputgrad guidedgrad gradcam \
+    -mt resnet resnetcbam resnetanr \
+    -down \
+    -bs 256 \
+    -ns 30 \
+    -cuda \
+    -skip1 \
+    -skip2 \
+    -sd 73 > ./trainlog/cifar10-resnetsmall-kar-rcd-eval.log &
